878d98086a5390e5ba02b0fe4ebf58f4003bc002
Jammin <AIEJam@LAPTOP-UVAQD524.ad.aie.edu>
2020-02-01 14:44:15 -0800

    Duct man player controller updated

--- Assets/Scripts/DuctManController.cs
+++ Assets/Scripts/DuctManController.cs
@@ -15,14 +15,19 @@ public class DuctManController : MonoBehaviour
     public float ForwardVelocity = 1f;
     public float CameraDistance = 3f;
     public Camera MainCameraReference;
-    public PlayerRunwayPosition PlayerPosition = PlayerRunwayPosition.Center;
+    public int PlayerPosition = 0;
     public Transform DuctTapeOuterTransform;
     public float TapeRotationalVelocityZ = 1f;
+    public float changeLaneDelay = 2.0f;
+    public float changeLaneSpeed = 0.1f;
 
     private Transform cameraTransform = null; 
     private Transform playerTransform = null;
     private float playerPositionX = 0f;
-    private PlayerRunwayPosition lastPosition = PlayerRunwayPosition.Center;
+    private float playerPositionY = 0f;
+    private Vector3 playerRotation = Vector3.zero;
+    private int lastPosition = 0;
+    private float timeSinceLaneChange = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -34,51 +39,195 @@ public class DuctManController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (lastPosition != PlayerPosition)
+        timeSinceLaneChange += Time.deltaTime;
+
+        switch (PlayerPosition)
         {
-            lastPosition = PlayerPosition;
-            switch (PlayerPosition)
-            {
-                case PlayerRunwayPosition.Left:
-                    playerPositionX = -2.25f;
-                    break;
-                case PlayerRunwayPosition.Center:
-                    playerPositionX = 0f;
-                    break;
-                case PlayerRunwayPosition.Right:
-                    playerPositionX = 2.25f;
-                    break;
-            }
+            case 0:
+                playerPositionX = 0f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
+            case 1:
+                playerPositionX = -2f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
+            case 2:
+                playerPositionX = -4f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
+            case 3:
+                playerPositionX = -6f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
+            case 4:
+                playerPositionX = -7.55f;
+                playerPositionY = 1f;
+                playerRotation = new Vector3(0, 0, -90);
+                break;
+            case 5:
+                playerPositionX = -7.55f;
+                playerPositionY = 3f;
+                playerRotation = new Vector3(0, 0, -90);
+                break;
+            case 6:
+                playerPositionX = -7.55f;
+                playerPositionY = 4.5f;
+                playerRotation = new Vector3(0, 0, -90);
+                break;
+            case 7:
+                playerPositionX = -7.55f;
+                playerPositionY = 6.25f;
+                playerRotation = new Vector3(0, 0, -90);
+                break;
+            case 8:
+                playerPositionX = -7.55f;
+                playerPositionY = 7f;
+                playerRotation = new Vector3(0, 0, -90);
+                break;
+            case 9:
+                playerPositionX = -4.5f;
+                playerPositionY = 9f;
+                playerRotation = new Vector3(0, 0, -180);
+                break;
+            case 10:
+                playerPositionX = -2.25f;
+                playerPositionY = 9f;
+                playerRotation = new Vector3(0, 0, -180);
+                break;
+            case 11:
+                playerPositionX = 0f;
+                playerPositionY = 9f;
+                playerRotation = new Vector3(0, 0, -180);
+                break;
+            case 12:
+                playerPositionX = 2.25f;
+                playerPositionY = 9f;
+                playerRotation = new Vector3(0, 0, -180);
+                break;
+            case 13:
+                playerPositionX = 4.5f;
+                playerPositionY = 9f;
+                playerRotation = new Vector3(0, 0, -180);
+                break;
+            case 14:
+                playerPositionX = 6.25f;
+                playerPositionY = 9f;
+                playerRotation = new Vector3(0, 0, -180);
+                break;
+            case 15:
+                playerPositionX = 7.5f;
+                playerPositionY = 7.5f;
+                playerRotation = new Vector3(0, 0, 90);
+                break;
+            case 16:
+                playerPositionX = 7.5f;
+                playerPositionY = 6.5f;
+                playerRotation = new Vector3(0, 0, 90);
+                break;
+            case 17:
+                playerPositionX = 7.5f;
+                playerPositionY = 4.5f;
+                playerRotation = new Vector3(0, 0, 90);
+                break;
+            case 18:
+                playerPositionX = 7.5f;
+                playerPositionY = 1.25f;
+                playerRotation = new Vector3(0, 0, 90);
+                break;
+            case 19:
+                playerPositionX = 6.25f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
+            case 20:
+                playerPositionX = 4.75f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
+            case 21:
+                playerPositionX = 2.5f;
+                playerPositionY = 0f;
+                playerRotation = Vector3.zero;
+                break;
         }
 
-        if (playerTransform.position.x != playerPositionX)
-        {
-            playerTransform.position = new Vector3(playerPositionX, playerTransform.position.y, playerTransform.position.z);
-        }
+        transform.position = Vector3.Lerp(transform.position, new Vector3(playerPositionX, playerPositionY, -6.066026f), changeLaneSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(playerRotation), changeLaneSpeed);
+        //Quaternion.Euler(playerRotation)
 
-        // update player & camera translation
-        playerTransform.position += new Vector3(0.0f, 0.0f, ForwardVelocity);
-        cameraTransform.position = new Vector3(playerPositionX, cameraTransform.position.y, playerTransform.position.z - CameraDistance);
+        #region Old system
+        //if (lastPosition != PlayerPosition)
+        //{
+        //    lastPosition = PlayerPosition;
+        //    //switch (PlayerPosition)
+        //    //{
+        //    //    case PlayerRunwayPosition.Left:
+        //    //        playerPositionX = -2.25f;
+        //    //        break;
+        //    //    case PlayerRunwayPosition.Center:
+        //    //        playerPositionX = 0f;
+        //    //        break;
+        //    //    case PlayerRunwayPosition.Right:
+        //    //        playerPositionX = 2.25f;
+        //    //        break;
+        //    //}
+        //}
 
-        //todo: this could be a function of player movement
-        DuctTapeOuterTransform.Rotate(new Vector3(0f, 0f, -TapeRotationalVelocityZ));
+        //if (playerTransform.position.x != playerPositionX)
+        //{
+        //    playerTransform.position = new Vector3(playerPositionX, playerTransform.position.y, playerTransform.position.z);
+        //}
+
+        //// update player & camera translation
+        //playerTransform.position += new Vector3(0.0f, 0.0f, ForwardVelocity);
+        //cameraTransform.position = new Vector3(playerPositionX, cameraTransform.position.y, playerTransform.position.z - CameraDistance);
+
+        ////todo: this could be a function of player movement
+        //DuctTapeOuterTransform.Rotate(new Vector3(0f, 0f, -TapeRotationalVelocityZ));
+
+        #endregion
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
+            Debug.Log("Test");
         if (context.performed)
         {
             var directionalMovement = context.ReadValue<Vector2>();
             var movementX = directionalMovement.x;
 
-            if (movementX > 0 && PlayerPosition < PlayerRunwayPosition.Right)
+
+            if (timeSinceLaneChange >= changeLaneDelay)
             {
-                PlayerPosition += 1;
+                if (movementX > 0)
+                {
+                    changeLaneDelay = 0f;
+                    PlayerPosition++;
+                }
+                else if (movementX < 0)
+                {
+                    changeLaneDelay = 0f;
+                    PlayerPosition--;
+                }
             }
-            else if (movementX < 0 && PlayerPosition > PlayerRunwayPosition.Left)
-            {
-                PlayerPosition -= 1;
-            }
+
+            if (PlayerPosition > 21)
+                PlayerPosition = 0;
+            if (PlayerPosition < 0)
+                PlayerPosition = 21;
+
+            //if (movementX > 0 && PlayerPosition < PlayerRunwayPosition.Right)
+            //{
+            //    PlayerPosition += 1;
+            //}
+            //else if (movementX < 0 && PlayerPosition > PlayerRunwayPosition.Left)
+            //{
+            //    PlayerPosition -= 1;
+            //}
         }
     }
 }
