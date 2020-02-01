aa0c8731e08e4d7fc7efb0653b5f71e991de000c
Adam Konig <akonig@me.com>
2020-02-01 14:36:56 -0800

    Color selection now works
    Color selection now works

---
+++ Assets/SetColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour
+{
+    public Material RedSkin;
+    public Material BlueSkin;
+    public Material ZebraSkin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer MR = GetComponent<MeshRenderer>();
+        string colorchoice = PlayerPrefs.GetString("Color", "Duct");
+        switch (colorchoice)
+        {
+            case "Red":
+                MR.material = RedSkin;
+                break;
+            case "Blue":
+                MR.material = BlueSkin;
+                break;
+            case "Zebra":
+                MR.material = ZebraSkin;
+                break;
+            default:
+                break;
+        }
+    }
+}
