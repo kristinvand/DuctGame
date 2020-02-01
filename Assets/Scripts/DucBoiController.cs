1f56dfc25849a33042a71e16f73ed2c78fdbb6ae
Patrick Godwin <patrick.godwin@outlook.com>
2020-01-31 22:48:26 -0800

    Add naive player controller with forward velocity and camera movement

---
+++ Assets/Scripts/DucBoiController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DucBoiController : MonoBehaviour
+{
+    public float ForwardVelocity = 1f;
+    public float CameraDistance = 3f;
+    public Camera MainCameraReference;
+
+    private Transform cameraTransform = null; 
+    private Transform playerTransform = null;
+
+    private int currentPlayerSlot = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = MainCameraReference.transform;
+        playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update player & camera translation
+        playerTransform.position += new Vector3(0.0f, 0.0f, ForwardVelocity);
+        cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, playerTransform.position.z - CameraDistance);
+    }
+}
