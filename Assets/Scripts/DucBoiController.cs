dec2eb787594f1016f1b1c93313a597c834c1028
Patrick Godwin <patrick.godwin@outlook.com>
2020-01-31 23:06:37 -0800

    Add X positioning to the player controller

--- Assets/Scripts/DucBoiController.cs
+++ Assets/Scripts/DucBoiController.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum PlayerRunwayPosition
+{
+    Default,
+    Left,
+    Center,
+    Right
+}
 
 public class DucBoiController : MonoBehaviour
 {
     public float ForwardVelocity = 1f;
     public float CameraDistance = 3f;
     public Camera MainCameraReference;
+    public PlayerRunwayPosition PlayerPosition = PlayerRunwayPosition.Center;
+
 
     private Transform cameraTransform = null; 
     private Transform playerTransform = null;
-
-    private int currentPlayerSlot = 0;
+    private float playerPositionX = 0f;
+    private PlayerRunwayPosition lastPosition = PlayerRunwayPosition.Default;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +34,31 @@ public class DucBoiController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (lastPosition != PlayerPosition)
+        {
+            lastPosition = PlayerPosition;
+            switch (PlayerPosition)
+            {
+                case PlayerRunwayPosition.Left:
+                    playerPositionX = -2.25f;
+                    break;
+                case PlayerRunwayPosition.Center:
+                    playerPositionX = 0f;
+                    break;
+                case PlayerRunwayPosition.Right:
+                    playerPositionX = 2.25f;
+                    break;
+
+            }
+        }
+
+        if (playerTransform.position.x != playerPositionX)
+        {
+            playerTransform.position = new Vector3(playerPositionX, playerTransform.position.y, playerTransform.position.z);
+        }
+
         // update player & camera translation
         playerTransform.position += new Vector3(0.0f, 0.0f, ForwardVelocity);
-        cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, playerTransform.position.z - CameraDistance);
+        cameraTransform.position = new Vector3(playerPositionX, cameraTransform.position.y, playerTransform.position.z - CameraDistance);
     }
 }
