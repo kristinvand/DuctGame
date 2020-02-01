fed196da170a136ed29f7c9ced159c7bdb21c7f9
ftrbreed <dan@future-breed.com>
2020-01-31 22:34:57 -0800

    Add some main menu buttons that work

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public enum SceneMapping
+    {
+        MainMenu = 0,
+        MainGame = 1
+    }
+
+    class MainMenuController : MonoBehaviour
+    {
+        public void LoadMainGame()
+        {
+            SceneManager.LoadScene((int)SceneMapping.MainGame);
+        }
+
+        public void ExitApplication()
+        {
+            Application.Quit();
+        }
+    }
+}
