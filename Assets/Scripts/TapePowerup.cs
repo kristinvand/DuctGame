fd7089bec72ddadb36c6b07b84a51bd175e328a7
Jammin <AIEJam@LAPTOP-UVAQD524.ad.aie.edu>
2020-02-01 00:47:36 -0800

    Pushing up ducktape pickup, with effect and script

---
+++ Assets/Scripts/TapePowerup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TapePowerup : MonoBehaviour
+{
+    public float refillAmount;
+
+    public float rotatationSpeed = 1.0f;
+
+    public GameObject pickupEffect;
+
+    public UnityEvent onPickup = new UnityEvent();
+
+    private void Update()
+    {
+        transform.Rotate(transform.up, rotatationSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //Add tape
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+
+            onPickup.Invoke();
+
+            Destroy(gameObject);
+        }
+    }
+}  
