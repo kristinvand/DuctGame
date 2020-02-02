f74ecfbba4acea5425c7a2ec57271bd219097d52
Jammin <AIEJam@LAPTOP-UVAQD524.ad.aie.edu>
2020-02-01 17:40:23 -0800

    Level generation working, particle FPS bug in motion

---
+++ Assets/Scripts/RoomMoving.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMoving : MonoBehaviour
+{
+    public float roomScrollSpeed = 0.1f;
+
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, roomScrollSpeed);
+
+        if (transform.position.z < -50f)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RoomSpawning.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawning : MonoBehaviour
+{
+    public float spawnInterval = 8.0f;
+
+    public GameObject emptyRoom;
+
+    public List<GameObject> rooms = new List<GameObject>();
+
+    [ReadOnly] public float timeUntilNextSpawn = 0f;
+    [ReadOnly] public GameObject lastSpawnRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeUntilNextSpawn = spawnInterval;
+
+        Instantiate(emptyRoom, new Vector3(0, 4.5f, 0f), Quaternion.identity);
+        Instantiate(emptyRoom, new Vector3(0, 4.5f, 21.5f), Quaternion.identity);
+        Instantiate(emptyRoom, new Vector3(0, 4.5f, 43f), Quaternion.identity);
+        Instantiate(emptyRoom, new Vector3(0, 4.5f, 64.5f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilNextSpawn += Time.deltaTime;
+
+        if(timeUntilNextSpawn >= spawnInterval)
+        {
+            lastSpawnRef = Instantiate
+            (
+                rooms[Random.Range(0, rooms.Count)], 
+                new Vector3(0, 4.5f, lastSpawnRef == null ? 86f : (lastSpawnRef.transform.position.z + 21.5f)), 
+                Quaternion.identity
+            );
+
+            timeUntilNextSpawn = 0f;
+        }
+    }
+}
