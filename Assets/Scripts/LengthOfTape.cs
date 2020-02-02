04ee0b1dfda333c4310dc9aedd34d0c67d7f09e0
Kristin <kristin.vandeusen@gmail.com>
2020-02-01 18:13:30 -0800

    updated script to include end statement for length of tape

--- Assets/Scripts/LengthOfTape.cs
+++ Assets/Scripts/LengthOfTape.cs
@@ -7,7 +7,7 @@ public class LengthOfTape : MonoBehaviour
     [SerializeField]
     GameObject ductTapeLength;
 
-    float emptyRollXValue = 0.0001768463f;
+    float emptyRollXValue = 0.0f;
     private Vector3 scaleChange = new Vector3(-0.001f, 0, 0f);
     private Vector3 positionChange = new Vector3(0f, 0, 0f);
 
