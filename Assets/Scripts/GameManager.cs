d4399719662fabf777517ae1b4a8239f86a940c7
Adam Konig <akonig@me.com>
2020-02-01 19:44:15 -0800

    Changes to things
    Changes to things

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    public int Points = 0;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void AddPoints(int delta)
+    {
+        Points += delta;
+    }
     // Start is called before the first frame update
     void Start()
     {
