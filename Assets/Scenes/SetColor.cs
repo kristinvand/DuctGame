66dfe81f2c4ec9e9276740872688a3a22d496f47
Adam Konig <akonig@me.com>
2020-02-02 10:12:50 -0800

    Moved things, downloaded fmod
    fmod

---
+++ Assets/Scenes/SetColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour
+{
+    public Material RedSkin;
+    public Material BlueSkin;
+    public Material ZebraSkin;
+
+    public LineRenderer LR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer MR = GetComponent<MeshRenderer>();
+        string colorchoice = PlayerPrefs.GetString("Color", "Duct");
+        switch (colorchoice)
+        {
+            case "Red":
+                MR.material = RedSkin;
+                LR.material = RedSkin;
+                break;
+            case "Blue":
+                MR.material = BlueSkin;
+                LR.material = BlueSkin;
+                break;
+            case "Zebra":
+                MR.material = ZebraSkin;
+                LR.material = ZebraSkin;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/LayTape.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayTape : MonoBehaviour
+{
+    LineRenderer LR;
+    public bool taping = false;
+    Vector3 LongTape = new Vector3(0, 0, -10);
+
+    public void Start()
+    {
+        LR = GetComponent<LineRenderer>();
+    }
+
+    public void EnableTape()
+    {
+        taping = true;
+    }
+
+    public void DisableTape()
+    {
+        taping = false;
+    }
+
+    private void Update()
+    {
+        if (taping)
+        {
+            LR.SetPosition(1, Vector3.Lerp(LR.GetPosition(1), LongTape, 0.1f));
+        }
+        else
+        {
+            LR.SetPosition(1, Vector3.Lerp(LR.GetPosition(1), Vector3.zero, 0.1f));
+        }
+    }
+}
